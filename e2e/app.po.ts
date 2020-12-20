91db2c6dd55dbf4346b5eecac16e1885504f994d
luanws <luan.w.silveira@gmail.com>
2020-12-20 15:53:48 -0300

    ponto e vírgula

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,11 @@
-import { browser, element, by } from 'protractor';
+import { browser, element, by } from 'protractor'
 
 export class MeatPage {
   navigateTo() {
-    return browser.get('/');
+    return browser.get('/')
   }
 
   getParagraphText() {
-    return element(by.css('mt-root h1')).getText();
+    return element(by.css('mt-root h1')).getText()
   }
 }
