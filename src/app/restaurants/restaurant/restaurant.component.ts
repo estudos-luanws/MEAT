19342133b5cd25c54c39129dcaf1e51561bf4482
luanws <luan.w.silveira@gmail.com>
2020-12-20 22:46:49 -0300

    restaurant

--- src/app/restaurants/restaurant/restaurant.component.ts
+++ src/app/restaurants/restaurant/restaurant.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, Input, OnInit } from '@angular/core'
+import { Restaurant } from './restaurant.model'
 
 @Component({
   selector: 'mt-restaurant',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./restaurant.component.css']
 })
 export class RestaurantComponent implements OnInit {
+  @Input()
+  restaurant: Restaurant
 
   constructor() { }
 
   ngOnInit() {
   }
-
 }
