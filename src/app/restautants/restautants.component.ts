4680de2d7f705f65e9eb3cc9bacac0c1408be00b
luanws <luan.w.silveira@gmail.com>
2020-12-20 22:15:38 -0300

    rotas

---
+++ src/app/restautants/restautants.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mt-restautants',
+  templateUrl: './restautants.component.html',
+  styleUrls: ['./restautants.component.css']
+})
+export class RestautantsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
