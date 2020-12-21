61926918471498a22014d6078b99b2f8f289ba11
luanws <luan.w.silveira@gmail.com>
2020-12-21 19:24:15 -0300

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,11 @@ import { HomeComponent } from './home/home.component'
 import { AboutComponent } from './about/about.component'
 import { RestaurantsComponent } from './restaurants/restaurants.component'
 import { RestaurantComponent } from './restaurants/restaurant/restaurant.component'
-import { RestaurantsService } from './restaurants/restaurants.service'
+import { RestaurantsService } from './restaurants/restaurants.service';
+import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
+import { MenuComponent } from './menu/menu.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { MenuItemComponent } from './menu-item/menu-item.component'
 
 
 @NgModule({
@@ -19,7 +23,11 @@ import { RestaurantsService } from './restaurants/restaurants.service'
     HomeComponent,
     AboutComponent,
     RestaurantsComponent,
-    RestaurantComponent
+    RestaurantComponent,
+    RestaurantDetailComponent,
+    MenuComponent,
+    ShoppingCartComponent,
+    MenuItemComponent
   ],
   imports: [
     BrowserModule,
--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -3,16 +3,17 @@ import { Http } from "@angular/http"
 import { MEAT_API } from "app.api"
 import { Observable } from "rxjs/Observable"
 import { Restaurant } from "./restaurant/restaurant.model"
+import { ErrorHandler } from '../app.error-handler'
 import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/catch'
 
 @Injectable()
 export class RestaurantsService {
-  constructor(private http: Http) {
-
-  }
+  constructor(private http: Http) { }
 
   restaurants(): Observable<Restaurant[]> {
     return this.http.get(`${MEAT_API}/restaurants`)
       .map(response => response.json())
+      .catch(ErrorHandler.handleError)
   }
 }
