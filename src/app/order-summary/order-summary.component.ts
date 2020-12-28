a415d40deac1a75cd2e4160d675226d95df16685
luanws <luan.w.silveira@gmail.com>
2020-12-28 15:04:04 -0300

    avaliação

--- src/app/order-summary/order-summary.component.ts
+++ src/app/order-summary/order-summary.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'mt-order-summary',
-  templateUrl: './order-summary.component.html',
-  styleUrls: ['./order-summary.component.css']
+    selector: 'mt-order-summary',
+    templateUrl: './order-summary.component.html',
+    styleUrls: ['./order-summary.component.css']
 })
 export class OrderSummaryComponent implements OnInit {
+    rated: boolean = false
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {
+    }
 }
--- src/app/shared/rating/rating.component.ts
+++ src/app/shared/rating/rating.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
-  selector: 'mt-rating',
-  templateUrl: './rating.component.html',
-  styleUrls: ['./rating.component.css']
+    selector: 'mt-rating',
+    templateUrl: './rating.component.html',
+    styleUrls: ['./rating.component.css']
 })
 export class RatingComponent implements OnInit {
+    rates: number[] = [1, 2, 3, 4, 5]
+    rate: number = 0
+    viewRate: number = 0
+    @Output() rated = new EventEmitter<number>()
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    setRate(rate: number) {
+        this.rate = rate
+        this.rated.emit(rate)
+    }
+
+    setViewRate(rate: number) {
+        this.viewRate = rate
+    }
+
+    clearViewRate() {
+        this.viewRate = this.rate
+    }
 }
