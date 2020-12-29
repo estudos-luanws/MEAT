6929687cc98ef3997b969887371a0c67ac6497f1
luanws <luan.w.silveira@gmail.com>
2020-12-29 14:57:33 -0300

    .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,6 @@ import { MenuComponent } from './restaurant-detail/menu/menu.component'
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component'
 import { RestaurantsComponent } from './restaurants/restaurants.component'
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
-import { OrderComponent } from './order/order.component'
 import { OrderSummaryComponent } from './order-summary/order-summary.component'
 
 export const ROUTES: Routes = [
@@ -18,7 +17,7 @@ export const ROUTES: Routes = [
             { path: 'reviews', component: ReviewsComponent },
         ]
     },
-    { path: 'order', component: OrderComponent },
     { path: 'order-summary', component: OrderSummaryComponent },
+    { path: 'order', loadChildren: './order/order.module#OrderModule' },
     { path: 'about', loadChildren: './about/about.module#AboutModule' },
 ]
