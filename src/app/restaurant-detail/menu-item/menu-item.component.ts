a798c9fa8212e65ad073ec13129d47b5868a36b4
luanws <luan.w.silveira@gmail.com>
2020-12-30 15:52:58 -0300

    animação menu item

--- src/app/restaurant-detail/menu-item/menu-item.component.ts
+++ src/app/restaurant-detail/menu-item/menu-item.component.ts
@@ -1,17 +1,27 @@
+import { animate, state, style, transition, trigger } from '@angular/animations'
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core'
 import { MenuItem } from './menu-item.model'
+
+type MenuItemAnimationState = 'ready' | 'void'
 
 @Component({
     selector: 'mt-menu-item',
     templateUrl: './menu-item.component.html',
-    styleUrls: ['./menu-item.component.css']
+    styleUrls: ['./menu-item.component.css'],
+    animations: [
+        trigger('menuItemAppeared', [
+            state('ready', style({ opacity: 1 })),
+            transition('void => ready', [
+                style({ opacity: 0, transform: 'translateY(-10px)' }),
+                animate('300ms 0s ease-in')
+            ])
+        ])
+    ]
 })
 export class MenuItemComponent implements OnInit {
-    @Input()
-    menuItem: MenuItem
-
-    @Output()
-    eventEmitter = new EventEmitter
+    @Input() menuItem: MenuItem
+    @Output() eventEmitter = new EventEmitter
+    menuItemAnimationState: MenuItemAnimationState = 'ready'
 
     constructor() { }
 
