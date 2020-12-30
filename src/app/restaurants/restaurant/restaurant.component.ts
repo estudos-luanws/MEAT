03b63159f92ead4c30eecc8bc6e75e3a962b081a
luanws <luan.w.silveira@gmail.com>
2020-12-30 15:49:11 -0300

    .

--- src/app/restaurants/restaurant/restaurant.component.ts
+++ src/app/restaurants/restaurant/restaurant.component.ts
@@ -1,17 +1,30 @@
+import { animate, state, style, transition, trigger } from '@angular/animations'
 import { Component, Input, OnInit } from '@angular/core'
 import { Restaurant } from './restaurant.model'
 
+type RestaurantAnimationState = 'ready' | 'void'
+
 @Component({
-  selector: 'mt-restaurant',
-  templateUrl: './restaurant.component.html',
-  styleUrls: ['./restaurant.component.css']
+    selector: 'mt-restaurant',
+    templateUrl: './restaurant.component.html',
+    styleUrls: ['./restaurant.component.css'],
+    animations: [
+        trigger('restaurantAppeared', [
+            state('ready', style({ opacity: 1 })),
+            transition('void => ready', [
+                style({ opacity: 0, transform: 'translate(-30px, -10px)' }),
+                animate('300ms 0s ease-in-out')
+            ])
+        ])
+    ]
 })
 export class RestaurantComponent implements OnInit {
-  @Input()
-  restaurant: Restaurant
+    @Input() restaurant: Restaurant
 
-  constructor() { }
+    restaurantAnimationState: RestaurantAnimationState = 'ready'
 
-  ngOnInit() {
-  }
+    constructor() { }
+
+    ngOnInit() {
+    }
 }
